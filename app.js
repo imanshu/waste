c65c0e7f98435d629a3882a95960eda18ec87d00
Tirumalesh12 <tirumaleshreddy12@gmail.com>
2017-01-02 14:22:24 +0530

    Updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+var bot = new builder.BotConnectorBot({ appId: 'db6c7eea-aac5-44ba-82b5-538c8827db30', appSecret: 'N2nZNmZ3mA4FCsygw15YSzi' });
 bot.add('/', function (session) {
     session.send('Hello World');
 	
