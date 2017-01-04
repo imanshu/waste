272573fb47563210f98f91157481277e3f7177bf
Tirumalesh12 <tirumaleshreddy12@gmail.com>
2017-01-04 13:37:13 +0530

    Updated

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'db6c7eea-aac5-44ba-82b5-538c8827db30', appSecret: 'N2nZNmZ3mA4FCsygw15YSzi' });
+var bot = new builder.BotConnectorBot({ appId: '0ff6befd-526d-4bd1-a8d4-ba2351d4fabe', appSecret: 'jGuCQczXEi34Pb51DwdLpMy' });
 bot.add('/', function (session) {
     session.send('Hello World');
-	
-	
 });
 
 // Setup Restify Server
