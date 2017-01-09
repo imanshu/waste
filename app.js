cb30b2db0b325a83f1bc1a8214f8edbde34df25a
Tirumalesh12 <tirumaleshreddy12@gmail.com>
2017-01-10 00:26:13 +0530

    Updated

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-
-var dialog = new builder.LuisDialog('https://api.projectoxford.ai/luis/v2.0/apps/56cb79d3-1f50-45fe-bee6-a7820ffe67ff?subscription-key=412111898d6f49a0b22467676f123ecb&verbose=true&q=');
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: '0ff6befd-526d-4bd1-a8d4-ba2351d4fabe', appSecret: 'jGuCQczXEi34Pb51DwdLpMy' });
-bot.add('/', dialog);
+
+var recognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v2.0/apps/56cb79d3-1f50-45fe-bee6-a7820ffe67ff?subscription-key=412111898d6f49a0b22467676f123ecb&verbose=true&q=');
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+
+bot.dialog('/', dialog);
 // Handling the Greeting intent. 
-dialog.on('Greeting', function (session, args) {
+dialog.matches('Greeting', function (session, args) {
 	console.log ('in greeting ');
 	session.send('Hello there! I am the notification bot. I can notify about the urgent orders');		
 });
 // Handling unrecognized conversations.
-dialog.on('None', function (session, args) {
+dialog.matches('None', function (session, args) {
 	console.log ('in none intent');	
 	session.send("I am sorry! I am a bot, perhaps not programmed to understand this command");			
 });
-dialog.on('Notify', function (session, args) {
+dialog.matches('Notify', function (session, args) {
 	console.log ('in notify ');
 	session.send('we just got an urgent order. Want to review it?');		
 });
