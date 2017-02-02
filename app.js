7c2a8dc51107e9aa43f724e9a80b5008d5d141dd
Tirumalesh12 <tirumaleshreddy12@gmail.com>
2017-02-03 01:12:15 +0530

    Updated

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: '0ff6befd-526d-4bd1-a8d4-ba2351d4fabe', appSecret: 'jGuCQczXEi34Pb51DwdLpMy' });
-
+var connector = new builder.ChatConnector({appId: process.env.MICROSOFT_APP_ID, appPassword: process.env.MICROSOFT_APP_PASSWORD});
+var bot = new builder.UniversalBot(connector);
 var recognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v2.0/apps/56cb79d3-1f50-45fe-bee6-a7820ffe67ff?subscription-key=412111898d6f49a0b22467676f123ecb&verbose=true&q=');
 var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', dialog);
 
-bot.add('/', dialog);
 // Handling the Greeting intent. 
 dialog.matches('Greeting', function (session, args) {
 	console.log ('in greeting ');
@@ -25,7 +25,7 @@ dialog.matches('Notify', function (session, args) {
 
 // Setup Restify Server
 var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.post('/api/messages', connector.listen());
 server.listen(process.env.port || 5000, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
