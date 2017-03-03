33a23c467e912bdc758bc137b91927b5dfbe93ac
Tirumalesh12 <tirumaleshreddy12@gmail.com>
2017-03-03 15:53:41 +0530

    Updated

--- app.js
+++ app.js
@@ -384,7 +384,7 @@ dialog.matches('Add Cart', function (session, args, results) {
 	}else {
 		callingApi(session.userData.path, function(data){	
 		addCart(session,data);
-		builder.Prompts.choice(session, "Select one option",['showcart','Continue Shopping']);
+		builder.Prompts.choice(session, "Select any one option",['Showcart','Continue Shopping']);
 		session.endDialog();
 		})
 	}
@@ -626,7 +626,15 @@ dialog.matches('Buy', [
         "merchant_name": "Walmart",		
         "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
         "timestamp":timeStamp, 
-        "elements": JSON.stringify(receipt, null, 4),
+        "elements": [
+          {
+            "title":"Classic White T-Shirt",
+            "subtitle":"100% Soft and Luxurious Cotton",
+            "quantity":2,
+            "price":50,
+            "currency":"USD",
+            "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png"
+          }],
         "address":{
           "street_1":"1 Hacker Way",
           "street_2":"",
