556e6a5e1d3c3bd97c5b62c472c7a9992cfdae84
Tirumalesh12 <tirumaleshreddy12@gmail.com>
2017-04-03 12:52:03 +0530

    updated

--- app.js
+++ app.js
@@ -21,6 +21,201 @@ WishMe = function(){
 	} 
 };
 
+promptThis = function(session){ 
+        if(session.userData.gender==""){
+			builder.Prompts.choice(session, "Please select the gender.",['Men','Women']);
+		}else if(session.userData.type==""){
+			builder.Prompts.choice(session, "It is very important to dress according to the occasion or the work you do. So what kind of shoe are you looking for?",['Dress','Casual','Athletic']);
+		}else if(session.userData.size==""){
+			builder.Prompts.choice(session, "What is the size you are looking for?",session.userData.sizes);
+		}else if(session.userData.brand==""){
+			session.beginDialog('/Brand');
+		}else if(session.userData.color==""){
+			builder.Prompts.choice(session, "Please select the color.",session.userData.colors);
+		}
+}
+
+deleteSpace = function(string){
+	var i = 1;
+	while(i>0){
+    i = string.indexOf(' ');
+    if(i>0){
+    string = string.substring(0, i) + string.substring(i+1, string.length);
+    }
+	}
+	return string
+}
+
+removeSpace = function(string){
+    var i = string.indexOf(' ');
+    if(i>0){
+    string = string.substring(0, i) + "%20" + string.substring(i+1, string.length);
+    }
+    var j = string.indexOf('\'');
+	if(j>0){
+		string = string.substring(0, j) + "%27" + string.substring(j+1, string.length);
+	}
+    return string;
+}
+
+choose_cat = function(gender, type){
+	    if (gender == "Women" && type == "Athletic"){
+		    category = "5438_1045804_1045806_1228540"
+	    }else if (gender == "Women" && type == "Casual"){
+		    category = "5438_1045804_1045806_1228545"
+	    }else if (gender == "Women" && ((type == "Formal")||(type == "Dress"))){
+		    category = "5438_1045804_1045806_1228546"
+	    }else if (gender == "Women" && type == ""){
+		    category = "5438_1045804_1045806"
+	    }else if (gender == "Men" && type == "Athletic"){
+		    category = "5438_1045804_1045807_1228548"
+	    }else if (gender == "Men" &&  type == "Casual"){
+		    category = "5438_1045804_1045807_1228552"
+	    }else if (gender == "Men" && ((type == "Formal")||(type == "Dress"))){
+		    category = "5438_1045804_1045807_1228553"
+	    }else if (gender == "Men" && type == ""){
+		    category = "5438_1045804_1045807"
+	    }else{
+		    category = "5438_1045804"}
+	return category;
+}
+
+capitalize = function(str) {
+    if(str != "dress"){
+	if (str !== null && str.length > 0 && (str.charAt(str.length-1)=='s')||(str.charAt(str.length-1)=='S')){
+	str = str.substring(0, str.length-1);
+	}}
+	str = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+	return str;
+}
+
+addCart = function(session, data){	
+	sess.maincart[sess.number] = { "title"    : data.name,
+					               "subtitle" : data.salePrice + '$',
+					               "image_url": data.thumbnailImage ,
+								   "buttons":[
+                                             {
+                                                "type":"postback",
+												"payload": data.salePrice + " remove item",
+                                                "title":"Remove item"
+                                             }  ]
+	               }
+  	session.userData.cartItem = sess.maincart;
+	sess.number += 1;
+	session.send("This item is been added to cart");
+}
+
+showItem = function(session, data){
+	session.send("Sure, Have a look here");
+	var item = new builder.Message(session)
+				.attachments([
+				new builder.HeroCard(session)
+		               .title(data.name)
+					   .subtitle(data.salePrice + '$, Customer Rating: '+data.customerRating+ ', Stock: '+data.stock )
+				       .images([
+					      builder.CardImage.create(session, data.largeImage),
+				         ])
+				       .buttons([
+					       builder.CardAction.postBack(session, "add item "+ parseInt(data.itemId) +" to cart","Add to Cart"),
+						   builder.CardAction.postBack(session, "Show more", "Show more"),
+						])
+				       ]);
+	session.send(item);
+}
+
+showoutput = function(session,data){
+	session.sendTyping();
+	var i=0;
+	var card = [];
+	if(!data.items){
+		session.send("Try another search. No product exists.")
+		session.endDialog();
+	}else{
+		while(data.items[i]){
+		card[i] =  new builder.HeroCard(session)
+		               .title(data.items[i].name)
+					   .subtitle(data.items[i].salePrice + '$')
+				       .images([
+					       builder.CardImage.create(session, data.items[i].thumbnailImage) 
+				       ])
+				       .buttons([
+					       builder.CardAction.postBack(session, "show item "+ parseInt(data.items[i].itemId),"Show item"),
+						   builder.CardAction.postBack(session, "add item "+ parseInt(data.items[i].itemId) +" to cart","Add to Cart")
+				       ])
+			    console.log("show item "+ parseInt(data.items[i].itemId));
+				i++;
+				}
+		if(data.items[9] !== undefined){	
+		card[i] = new builder.HeroCard(session)
+                      .subtitle('Want to see Similar kind of shoes? Click below')
+                      .buttons([
+					       builder.CardAction.imBack(session, "Show more", "Show more"),
+				       ])
+		}
+		session.userData.colors = colorsArray(session, data);
+		session.userData.brands = brandsArray(session, data);
+		session.userData.sizes = sizesArray(session, data);
+		var msg = new builder.Message(session)
+				.attachmentLayout(builder.AttachmentLayout.carousel)
+				.attachments(card);
+				session.send(msg);
+	}			
+}
+
+brandsArray = function(session,data){
+    brands = [];	
+	var j=0;
+	var k=0;
+	while(data.facets[j]){
+	if(data.facets[j].name == "brand"){
+		while((data.facets[j].facetValues[k])&&(k<9)){
+			brands[k] = data.facets[j].facetValues[k].name;
+			k++;						
+			}
+		break;
+	}
+	j++;
+	}
+	brands.push("Any Brand");
+	return brands;
+}
+
+sizesArray = function(session,data){
+    sizes = [];	
+	var j=0;
+	var k=0;
+	while(data.facets[j]){
+	if(data.facets[j].name == "shoe_size"){
+		while((data.facets[j].facetValues[k])&&(k<9)){
+			sizes[k] = data.facets[j].facetValues[k].name;
+			k++;						
+			}
+		break;
+	}
+	j++;
+	}
+	sizes.push("Any Size");
+	return sizes;
+}
+
+colorsArray = function(session,data){
+    colors = [];	
+	var j=0;
+	var k=0;
+	while(data.facets[j]){
+	if(data.facets[j].name == "color"){
+		while((data.facets[j].facetValues[k])&&(k<9)){
+			colors[k] = data.facets[j].facetValues[k].name;
+			k++;						
+			}
+		break;
+	}
+	j++;
+	}
+	colors.push("Any Color");
+	return colors;
+}
+
 weatherApi = function(place, callback){
     var options = {
 		host: 'api.openweathermap.org',
